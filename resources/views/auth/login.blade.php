ac390affa948ced7da1cfe37711d1e5f620c96c4
EC2 Default User <ec2-user@ip-172-31-10-198.ec2.internal>
2021-12-25 17:57:33 +0900

    ログイン・サインイン画面の修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.not_logged_in')
  
 @section('content')
-  <h1>ログイン</h1>
- 
-  <form method="POST" action="{{ route('login') }}">
-      @csrf
-      <div>
-          <label>
-            メールアドレス:
-            <input type="email" name="email">
-          </label>
-      </div>
- 
-      <div>
-          <label>
-            パスワード:
-            <input type="password" name="password" >
-          </label>
-      </div>
- 
-      <input type="submit" value="ログイン">
-  </form>
+  <div class = "top">
+    <h1>ログイン</h1>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class = "top_input">
+          <label>メールアドレス</label>
+          <input type="email" name="email">
+          
+          <label>パスワード</label>
+          <input type="password" name="password" >
+          
+          <input class = "button" type="submit" value="ログイン">
+          <p>会員登録がまだの方は<a href="{{ route('register') }}">こちら</a></p>
+        </div>
+    </form>
+  </div>
 @endsection
