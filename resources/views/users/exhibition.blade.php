6663366da76ea2e7d3f37caa6f7c7603f7fd8a04
EC2 Default User <ec2-user@ip-172-31-10-198.ec2.internal>
2022-01-04 00:27:41 +0900

    販売状況の修正

--- resources/views/users/exhibition.blade.php
+++ resources/views/users/exhibition.blade.php
@@ -10,12 +10,19 @@
             <div class="item_content">
               <div class="item_header">
                 <a href="{{ route('items.show', $item->id)}}"><img src="{{ asset('storage/' . $item->image) }}"></a>
-                <p>{{ $item->description }}</p>
+                <!--<p>{{ $item->description }}</p>-->
               </div>
               <div class="item_body">
+                @if( $item->isOrderedBy() === false)
+                  <p class = "on_sale">出展中</p>
+                @else
+                  <p class = "sold_out">売切れ</p>
+                @endif
                 <p>商品名：{{ $item->name }} {{ $item->price }}円</p>
-                <p>カテゴリ：{{ $item->category->name }} [{{ $item->created_at }}]</p>
-                <p>{{ $item->isOrderedBy() ? '売り切れ' : '出品中' }}</p>
+                <p>カテゴリ：{{ $item->category->name }}</p>
+                 <p>{{ $item->description }}</p>
+                <p>[{{ $item->created_at }}]</p>
+                <!--<p>{{ $item->isOrderedBy() ? '売り切れ' : '出品中' }}</p>-->
                 [<a href="{{ route('items.edit', $item) }}">編集</a>]
                 [<a href="{{ route('items.edit_image', $item) }}">画像を変更</a>]
                 <form class="delete" method="post" action="{{ route('items.destroy', $item) }}">
